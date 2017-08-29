c473b4f4b5320d55957cfec3803415b261416b00
hardish wilkhu <hardish_95@hotmail.co.uk>
2017-08-29 03:05:09 +0100

    changed port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require("body-parser");
 var routes = require("./api/routes");
 
 // Set port to listen on
-app.set("port", 80);
+app.set("port", 3001);
 
 // Middleware to log incoming requests
 app.use(function(req, res, next){
@@ -14,19 +14,19 @@ app.use(function(req, res, next){
   next();
 });
 
-// Static dirctories
+// Static directories
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use("/node_modules", express.static(__dirname + "/node_modules"));
 
 // Enable parsing of posted forms
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// =Routing
-app.use('/api', routes);
+// Routing
+app.use("/api", routes);
 
 // Listen for requests
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get("port"), function() {
   var port = server.address().port;
-  console.log('Magic happens on port ' + port);
+  console.log("Magic happens on port " + port);
 });
