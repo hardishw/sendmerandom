0efd5f3491c06b30427031a90f8705cc7dea267d
hardish wilkhu <hardish_95@hotmail.co.uk>
2017-08-31 15:37:22 +0100

    updates

--- public/angular-app/item-display/item.controller.js
+++ public/angular-app/item-display/item.controller.js
@@ -3,7 +3,10 @@ angular.module("sendmerandom").controller("ItemController", ItemController);
 function ItemController($route, $routeParams, itemDataFactory) {
   var vm = this;
   var itemId = $routeParams.itemId
-  vm.button = "Buy Me";
+  var fivePound = "B5CCBH8THMCCW"
+  var tenPound = "6P3WQSGSTLKPQ"
+
+  vm.button = "Add To Cart";
 
   console.log(itemId);
 
@@ -12,6 +15,11 @@ function ItemController($route, $routeParams, itemDataFactory) {
     vm.item = response.data;
     if (vm.item.stock == 0) {
       vm.button = "Out of Stock";
+    }
+    if (vm.item.price == 10) {
+      vm.pricecode = tenPound;
+    } else if (vm.item.price == 5) {
+      vm.pricecode = fivePound
     }
   });
 }
