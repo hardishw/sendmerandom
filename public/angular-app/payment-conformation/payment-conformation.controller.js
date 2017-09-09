a6fff51ee3db995a1f1b87e28734750138e2460f
hardish wilkhu <hardish_95@hotmail.co.uk>
2017-09-09 23:04:53 +0100

    pdt conformation page

--- public/angular-app/payment-conformation/payment-conformation.controller.js
+++ public/angular-app/payment-conformation/payment-conformation.controller.js
@@ -1,22 +1,29 @@
 angular.module("sendmerandom").controller("PaymentConformation",PaymentConformation);
 
-var _splitArray = function(input){
-  var output
-
-  if (input && input.length > 0){
-    output = input.split("\n");
-  } else {
-    output = [];
-  }
-
-  return output;
-}
-
 function PaymentConformation($http,$location) {
   var vm = this;
   var tx = $location.search().tx;
+  var pdt = {};
+  vm.cart = [];
 
   $http.get("/api/pdt/" + tx).then(function (response) {
     vm.output = response.data.split("\n");
+
+    for (property in response.data.split("\n")){
+      var key = response.data.split("\n")[property].split("=")[0];
+      var value = response.data.split("\n")[property].split("=")[1];
+
+      pdt[key] = value;
+    }
+
+    for (i= 1; i < pdt["num_cart_items"];i++){
+      vm.cart[i-1]=pdt["item_name"+i];
+    }
+
   });
+
+  vm.payment_status = vm.output[0];
+
+
+
 }
