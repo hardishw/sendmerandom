621c53412327e96d959b6d2560ad25df535f40f2
hardish wilkhu <hardish_95@hotmail.co.uk>
2017-09-10 00:03:05 +0100

    confirmation page address

--- public/angular-app/payment-conformation/payment-conformation.controller.js
+++ public/angular-app/payment-conformation/payment-conformation.controller.js
@@ -3,7 +3,7 @@ angular.module("sendmerandom").controller("PaymentConformation",PaymentConformat
 function PaymentConformation($http,$location) {
   var vm = this;
   var tx = $location.search().tx;
-  var pdt = {};
+  vm.pdt = {};
   vm.cart = [];
 
   $http.get("/api/pdt/" + tx).then(function (response) {
@@ -13,10 +13,10 @@ function PaymentConformation($http,$location) {
       var key = response.data.split("\n")[property].split("=")[0];
       var value = response.data.split("\n")[property].split("=")[1];
 
-      pdt[key] = value;
+      vm.pdt[key] = value;
     }
 
-    for (i= 1; i < parseInt(pdt["num_cart_items"]) + 1;i++){
+    for (i= 1; i < parseInt(vm.pdt["num_cart_items"]) + 1;i++){
       var item = {};
       item["name"]=pdt["item_name"+i];
       item["qty"]=pdt["quantity"+i]
